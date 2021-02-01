2a0e6bd5640365e2e63c54cf1edf098fee3af0c3
triis <56828795+triis@users.noreply.github.com>
2021-02-01 11:58:30 +0100

    add first overview

--- zurich_walks_script.R
+++ zurich_walks_script.R
@@ -1,5 +1,12 @@
 #load packages
-if(!require(dplyr)){install.packages('dplyr')}
+if (!require(dplyr)) {
+  install.packages('dplyr')
+}
+if (!require(ggplot2)) {
+  install.packages('ggplot2')
+}
+
+
 
 #load data
 zwalks = read.csv("2020_verkehrszaehlungen_werte_fussgaenger_velo.csv")
@@ -14,21 +21,33 @@ zwalks <- setNames(zwalks, tolower(names(zwalks)))
 zwalks[is.na(zwalks)] <- 0
 
 #rename columns
-zwalks <- zwalks %>% 
-  rename(
-    datetime = datum
-  )
+zwalks <- zwalks %>%
+  rename(datetime = datum)
 
 
 #summarize data on a daily basis
-zwalks.day <- aggregate(zwalks[c("velo_in","velo_out","fuss_in","fuss_out")],
-                               by = zwalks[c("fk_zaehler","fk_standort", "date","ost", "nord")],
-                               FUN=sum
-                        )
+zwalks.day <-
+  aggregate(zwalks[c("velo_in", "velo_out", "fuss_in", "fuss_out")],
+            by = zwalks[c("fk_zaehler", "fk_standort", "date", "ost", "nord")],
+            FUN = sum)
 
+
+zwalks.day['fuss_all'] <- zwalks.day$fuss_in + zwalks.day$fuss_out
+zwalks.day['velo_all'] <- zwalks.day$velo_in + zwalks.day$velo_out
+zwalks.day['person_all'] <-
+  zwalks.day$velo_all + zwalks.day$fuss_all
 
 
 #geographical data
 
 #install.packages("rgdal")
 #ibrary(rgdal)
+
+#look at data
+# plot air temp
+
+ggplot(zwalks.day, aes(x=date, y=person_all)) +
+  geom_point(na.rm = TRUE, size = 0.75) +
+  ggtitle("Pedestrian and Bicycles in Zurich through 2020") +
+  xlab("2020") +
+  ylab("Pedestrians and Bicycles")
